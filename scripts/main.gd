9b938fd1c9db572ebf5bf4d696907194a89f4812
Alexey Korepanov <alexey.korepanov@clickhouse.com>
2026-01-01 14:14:22 +0100

    Add girl spritesheet and scene; update main scene to include boy instance

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,10 +1,23 @@
 extends Node2D
 
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@export var move_speed := 120.0
+
+@onready var boy: Node2D = $Boy
+@onready var boy_animation: AnimationPlayer = $Boy/AnimationPlayer
 
 func _ready() -> void:
-	animated_sprite.play("run")
-	_center_sprite()
+	_center_boy()
 
-func _center_sprite() -> void:
-	animated_sprite.position = get_viewport_rect().size / 2.0
+func _process(delta: float) -> void:
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction != 0.0:
+		boy.position.x += direction * move_speed * delta
+		if boy_animation.current_animation != "walk":
+			boy_animation.play("walk")
+		boy.scale.x = abs(boy.scale.x) * (-1.0 if direction > 0.0 else 1.0)
+	else:
+		if boy_animation.current_animation != "RESET":
+			boy_animation.play("RESET")
+
+func _center_boy() -> void:
+	boy.position = get_viewport_rect().size / 2.0
